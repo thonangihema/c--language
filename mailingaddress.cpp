1c0ef1d33e4f28ca5091a82e3f73faa588c4ced5
T.Hema 21wh5a0502 <21wh5a0502@bvrithyderabad.edu.in>
2022-12-02 21:47:26 +0530

    Create mailingaddress.cpp

---
+++ mailingaddress.cpp
@@ -0,0 +1,9 @@
+/*Write a program that displays your name and complete mailing address. The address
+should be printed in the format that is normally used in the area where you live. Your
+program does not need to read any input from the user*/
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"T.Hema\n9-81/70,P.R.Nagar,Borbonda \n Hyderabad-500038";
+}
