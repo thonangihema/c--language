9209ddcb1c1d221df73bedfa5a59e6544ffa763f
T.Hema 21wh5a0502 <21wh5a0502@bvrithyderabad.edu.in>
2022-12-03 15:08:58 +0530

    Update sum_of_n.cpp

--- sum_of_n.cpp
+++ sum_of_n.cpp
@@ -1,3 +1,6 @@
+/*Write a program that reads a positive integer, n, from the user and then displays the sum 
+of all of the integers from 1 to n. The sum of the first n positive integers can be computed 
+using the formula*/
 #include<iostream>
 using namespace std;
 int main()
