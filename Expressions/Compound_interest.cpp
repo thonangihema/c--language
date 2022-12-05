ee8840fff8e1e0d72585b63f280eb1cc7ca844c6
T.Hema 21wh5a0502 <21wh5a0502@bvrithyderabad.edu.in>
2022-12-05 22:56:01 +0530

    Create Compound_interest.cpp

---
+++ Expressions/Compound_interest.cpp
@@ -0,0 +1,23 @@
+/*Pretend that you have just opened a new savings account that earns 4 percent interest per 
+year. The interest that you earn is paid at the end of the year, and is added to the balance 
+of the savings account. Write a program that begins by reading the amount of money 
+deposited into the account from the user. Then your program should compute and display 
+the amount in the savings account after 1, 2, and 3 years. Display each amount so that it 
+is rounded to 2 decimal places.
+*/
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int m;
+    float one=0.0,two=0.0,three=0.0;
+    cout<<"Enter the amount of money deposited into the account : ";
+    cin>>m;
+    one=(float)4/100+m;
+    two=(float)4/100+one;
+    three=(float)4/100+two;
+    cout<<"Year-1 : " <<one<<"\n";
+    cout<<"Year-2 : " <<two<<"\n";
+    cout<<"Year-3 : " <<three<<"\n";
+
+}
