40a2ad30358ad99bb629a1ecc5ade537900ee4dd
T.Hema 21wh5a0502 <21wh5a0502@bvrithyderabad.edu.in>
2022-12-07 10:58:25 +0530

    Create License_plate_Valid_or_not.cpp

---
+++ Decision_Making/License_plate_Valid_or_not.cpp
@@ -0,0 +1,41 @@
+/*In a particular jurisdiction, older license plates consist of three uppercase letters followed 
+by three digits. When all of the license plates following that pattern had been used, the 
+format was changed to four digits followed by three uppercase letters. Write a program 
+that begins by reading a string of characters from the user. Then your program should 
+display a message indicating whether the characters are valid for an older style license 
+plate or a newer style license plate. Your program should display an appropriate message 
+if the string entered by the user is not valid for either style of license plate.*/
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+ string s;
+  cout<<"Enter string of characters : ";
+  cin>>s;
+  if(s.length()==6)
+  {
+    if(((s[0]<91)&&(s[0]>64))&&((s[1]<91)&&(s[1]>64))&&((s[2]<91)&&(s[2]>64))&&((s[3]<58)&&(s[3]>48))&&((s[4]<58)&&(s[4]>48))&&((s[5]<58)&&(s[5]>48)))
+    {
+        cout<<"Valid";
+    }
+    else
+    {
+        cout<<"Not Valid";
+    }
+  }
+  else if(s.length()==7)
+  {
+    if(((s[0]<58)&&(s[0]>48))&&((s[1]<58)&&(s[1]>48))&&((s[2]<58)&&(s[2]>48))&&((s[3]<58)&&(s[3]>48))&&((s[4]<91)&&(s[4]>64))&&((s[5]<91)&&(s[5]>64))&&((s[6]<91)&&(s[6]>64)))
+    {
+      cout<<"Valid";
+    }
+    else
+    {
+        cout<<"Not Valid";
+    }
+  }
+  else
+  {
+    cout<<"Not Valid";
+  }
+}
