559d8bf560bae608f61ee28303d3de7500125d5c
T.Hema 21wh5a0502 <21wh5a0502@bvrithyderabad.edu.in>
2022-12-11 18:22:14 +0530

    Create Decimal_to_binary.cpp

---
+++ repetitions-Simple_Loops/Decimal_to_binary.cpp
@@ -0,0 +1,36 @@
+/*
+Write a program that converts a decimal (base 10) number to binary (base 2). Read the
+decimal number from the user as an integer and then use the division algorithm shown
+below to perform the conversion. When the algorithm completes, result contains the
+binary representation of the number. Display the result, along with an appropriate
+message.
+Let result be an empty string
+Let q represent the number to convert
+repeat
+Set r equal to the remainder when q is divided by 2
+Convert r to a string and add it to the beginning of result
+Divide q by 2, discarding any remainder, and store the result back into q
+until q is 0 
+*/
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int q,r,dis;
+    vector<int>result;
+    cin>>q;
+    dis=q;
+    while(q!=0)
+    {
+        r=q%2;
+        result.push_back(r);
+        q=q/2;
+
+    }
+    cout<<"Binary Equivalent to decimal "<<dis<<" is ";
+    for(int i=result.size()-1;i>=0;i--)
+    {
+        cout<<result[i];
+    }
+
+}
