460dad598ccbd2ad5c2a6f04bdd48106502aa8f0
T.Hema 21wh5a0502 <21wh5a0502@bvrithyderabad.edu.in>
2022-12-16 10:15:10 +0530

    Create Is_Prime.cpp

---
+++ functions1/Is_Prime.cpp
@@ -0,0 +1,39 @@
+/*A prime number is an integer greater than 1 that is only divisible by one and itself. Create
+a function named isPrime that determines whether or not its parameter is prime, returning
+true if it is, and false otherwise. Write a main program that reads an integer from the user 
+and displays a message indicating whether or not it is prime.
+*/
+#include<bits/stdc++.h>
+using namespace std;
+bool isPrime(int num)
+{
+    int count=0;
+    for(int i=2;i<=sqrt(num);i++)
+   {
+      if(num%i==0)
+      {
+         count=1;
+         break;
+      }
+   }
+   if(count==0 && num>1)
+   {
+      return true;
+   }
+   else{
+      return false;
+   }
+    
+}
+int main()
+{
+   int number;
+   cin>>number;
+   if(isPrime(number)){
+       cout<<"Prime number";
+   }
+   else
+   {
+       cout<<"Not a prime number";
+   }
+   }
