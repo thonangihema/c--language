6875bc23a6057f08da156a9ef5ce18cc32c773cd
T.Hema 21wh5a0502 <21wh5a0502@bvrithyderabad.edu.in>
2022-12-27 22:26:20 +0530

    Update Random_License_Plate.cpp

--- Randomness/Simulation/Random_License_Plate.cpp
+++ Randomness/Simulation/Random_License_Plate.cpp
@@ -4,7 +4,8 @@ changed to four digits followed by three letters. Create a function named
 randomLicencePlate that generates a random license plate. Your function should have
 approximately equal odds of generating a sequence of characters for an old license plate 
 or a new license plate. Write a main program that calls your function and displays the 
-randomly generated license plate.*/
+randomly generated license plate.
+*/
 #include<bits/stdc++.h>
 using namespace std;
 string randomLicencePlate()
