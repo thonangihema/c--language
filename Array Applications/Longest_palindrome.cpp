f57f259e1f07d6539c1fb2d53cbb1a88fd26836a
T.Hema 21wh5a0502 <21wh5a0502@bvrithyderabad.edu.in>
2023-01-03 23:00:07 +0530

    Create Longest_palindrome.cpp

---
+++ Array Applications/Longest_palindrome.cpp
@@ -0,0 +1,73 @@
+/*Given a string s which consists of lowercase or uppercase letters, return the length of the 
+longest palindrome that can be built with those letters. Assume that letters are case 
+sensitive, for example, "Aa" is not considered a palindrome here.
+Input: s = "abccccdd"
+Output: 7*/
+#include<bits/stdc++.h>
+using namespace std;
+int longPalindrome(string word)
+{
+    map<char, int> arr ;
+        for (char x:word)
+         {
+            arr[x]++;
+        }
+int c=0,freq=0,carry=0;
+for(int i=0;i<arr.size();i++)
+{
+    if(arr[i]%2==0)
+    {
+        c+=arr[i];
+    }
+    else
+    {
+        freq=max(freq,arr[i]);
+        c+=arr[i]-1;
+    }
+}
+
+if(freq>=1)
+{
+    c++;
+}
+
+return c;
+/* int arr[52]={0};
+for(auto s:word)
+{
+    if(s>='a' && s<='z')
+    {
+        ++arr[s-'a'+6];
+    }
+    else
+    {
+        ++arr[s-'A'];
+    } 
+}
+int c=0,freq=0;
+for(int i=0;i<52;i++)
+{
+    if(arr[i]%2==0)
+    {
+        c+=arr[i];
+    }
+    else
+    {
+        freq=max(freq,arr[i]);
+        c+=arr[i]-1;
+    }
+}
+if(freq>=1)
+{
+    c++;
+}
+return c; */
+
+}
+int main()
+{
+    string s;
+         cin>>s;
+        cout<<longPalindrome(s);
+        cout<<"\n";
+}
